de7cfb1c5f9d04658e761d4bf9792070f370c216
rsancho64 <rsancho@iesrioarba.es>
2022-01-25 14:12:58 +0100

    .

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,11 +3,14 @@ const router = require('express').Router();
 const customerController = require('../controllers/customerController');
 
 // CRUD
-router.post('/add',        customerController.save);    // C
-router.get ('/',           customerController.list);    // R 
+//router.post('/add',        customerController.save);    // C
+router.post('/customer/add',        customerController.save);    // C
+
+//router.get ('/',           customerController.list);    // R 
+router.get ('/customer',           customerController.list);    // R 
+
 router.get ('/update/:id', customerController.edit);    // U
 router.post('/update/:id', customerController.update);  // U
 router.get ('/delete/:id', customerController.delete);  // D
 
 module.exports = router;
-

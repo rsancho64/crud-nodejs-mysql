fcd3e857f6777d8ee080c114d7719263277c5005
rsancho64 <rsancho@iesrioarba.es>
2022-01-25 13:28:58 +0100

    .

--- database/db.sql
+++ database/db.sql
@@ -6,10 +6,18 @@ use crudnodejsmysql;
 -- creating a new table
 CREATE TABLE customer (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  phone VARCHAR(15)
+  name    VARCHAR(50)  NOT NULL,
+  address VARCHAR(100) , -- NOT NULL,
+  phone   VARCHAR(15)
 );
+
+insert into customer( name, address ) values
+("marta", "mayor 4"),
+("maria", "gran via 22");
+
+insert into customer( id, name ) values
+(100, "laura"),
+(666, "luz");
 
 -- to show all tables
 show tables;
